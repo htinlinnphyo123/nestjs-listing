7a85a20e694355be74b427aa5f2cfc9dc434639e
Htin Linn Phyo <htinlinnphyo.npt@gmail.com>
2026-01-28 22:21:14 +0630

    remove dist

--- dist/src/listings/dto/update-listing.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateListingDto } from './create-listing.dto';
-declare const UpdateListingDto_base: import("@nestjs/common").Type<Partial<CreateListingDto>>;
-export declare class UpdateListingDto extends UpdateListingDto_base {
-}
-export {};
--- dist/src/users/entities/user.entity.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class UserEntity {
-    id: number;
-    email: string;
-    name: string;
-    createdAt: Date;
-    updatedAt: Date;
-    constructor(partial: Partial<UserEntity>);
-}
